88115e5a811ca4a8e4e9afcec8ef82112aaf5d45
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-09-19 07:18:15 +0900

    feat: update view

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const res = require("express/lib/response");
+const response = require("express/lib/response");
 const http = require("http");
-
 const app = express();
+
+const fs = require("fs");
+
+let user;
+fs.readFile("database/users.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("System error is happened!");
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -11,9 +21,13 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  return res.json({ test: "success" });
+app.post("/create-item", (request, response) => {
+  console.log(request.body);
+  return response.json({ test: "success" });
+});
+
+app.get("/author", (request, response) => {
+  return response.render("author", { user: user });
 });
 
 app.get("/", (req, res) => {
